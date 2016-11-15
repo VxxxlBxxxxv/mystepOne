dcb4dfbb80be681c33f0a339e391d42e56e79f46
Vasyl <barkov.vasiliy.84@gmail.com>
2016-11-15 09:23:32 +0300

    stage2

--- src/test/java/com/mytest/app/SampleSauceTest.java
+++ src/test/java/com/mytest/app/SampleSauceTest.java
@@ -1,16 +1,15 @@
 
 import com.codeborne.selenide.WebDriverRunner;
-import com.fasterxml.jackson.annotation.JsonFormat;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import sun.awt.SunHints;
 
 import java.net.URL;
 
 import static com.codeborne.selenide.Condition.*;
 import static com.codeborne.selenide.Selectors.byCssSelector;
+import static com.codeborne.selenide.Selectors.byXpath;
 import static com.codeborne.selenide.Selenide.*;
 
 
@@ -72,6 +71,41 @@ public class SampleSauceTest {
         $("div.GNHGC04CCB:nth-child(1)").sendKeys(Keys.chord(Keys.END));
         $("div.GNHGC04CCB:nth-child(251)").shouldNot(exist);
 
+
+
+
+
+//тестируем редактирование в строке
+        open("http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCellTable");
+        $(byCssSelector("tr.GNHGC04CBD:nth-child(1) > td:nth-child(2) > div:nth-child(1)")).doubleClick();
+        $(byCssSelector("tr.GNHGC04CBD:nth-child(1) > td:nth-child(2) > div:nth-child(1)")).$(byXpath("./input")).setValue("John");
+        $(byCssSelector("tr.GNHGC04CBD:nth-child(1) > td:nth-child(2) > div:nth-child(1)")).pressTab();
+        $(byCssSelector("tr.GNHGC04CBD:nth-child(1) > td:nth-child(3) > div:nth-child(1)")).doubleClick();
+        $(byCssSelector("tr.GNHGC04CBD:nth-child(1) > td:nth-child(3) > div:nth-child(1)")).$(byXpath("./input")).setValue("Wayne");
+        $(byCssSelector("tr.GNHGC04CBD:nth-child(1) > td:nth-child(3) > div:nth-child(1)")).pressTab();
+        $(byCssSelector("tr.GNHGC04CBD:nth-child(1) > td:nth-child(2) > div:nth-child(1)")).shouldHave(exactText("John"));
+        $(byCssSelector("tr.GNHGC04CBD:nth-child(1) > td:nth-child(3) > div:nth-child(1)")).shouldHave(exactText("Wayne"));
+
+
+
+//тестируем переход по страницам
+        open("http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCellTable");
+//вперед
+        $(byCssSelector("td.GNHGC04CLH:nth-child(4) > img:nth-child(1)")).click();
+        $(byCssSelector("div.gwt-HTML:nth-child(1)")).shouldHave(text("16-30 of 250"));
+        $(byCssSelector("td.GNHGC04CLH:nth-child(5) > img:nth-child(1)")).click();
+        $(byCssSelector("div.gwt-HTML:nth-child(1)")).shouldHave(text("241-250 of 250"));
+//назад
+        $(byCssSelector("td.GNHGC04CLH:nth-child(2) > img:nth-child(1)")).click();
+        $(byCssSelector("div.gwt-HTML:nth-child(1)")).shouldHave(text("226-240 of 250"));
+        $(byCssSelector("td.GNHGC04CLH:nth-child(1) > img:nth-child(1)")).click();
+        $(byCssSelector("div.gwt-HTML:nth-child(1)")).shouldHave(text("1-15 of 250"));
+
+
+//тестируем сохранение состояния чекбокса при смене страниц и сортировки
+        open("http://samples.gwtproject.org/samples/Showcase/Showcase.html#!CwCellTable");
+        $(byCssSelector("th.GNHGC04CHD:nth-child(2)")).click();
+        $(byCssSelector("tr.GNHGC04CBD:nth-child(1) > td:nth-child(1) > div:nth-child(1) > input:nth-child(1)")).click();
 
 
 
